a4abbebbc32e9206c4db76028bb701bb9236a221
Red_Epicness <redepicness1@gmail.com>
2015-11-19 16:23:31 +0100

    Initial commit

---
+++ Bukkit/src/me/redepicness/serverstatus/bukkit/main.java
@@ -0,0 +1,12 @@
+package me.redepicness.serverstatus.bukkit;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class main extends JavaPlugin{
+
+    @Override
+    public void onEnable() {
+
+    }
+
+}
---
+++ Bungee/src/me/redepicness/serverstatus/bungee/main.java
@@ -0,0 +1,12 @@
+package me.redepicness.serverstatus.bungee;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public class main extends Plugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+}

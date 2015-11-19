3453a87af91f54f4be8f0b00fb1d0e614647745d
Red_Epicness <redepicness1@gmail.com>
2015-11-19 17:12:31 +0100

    Start on plugin base.

--- Bukkit/src/me/redepicness/serverstatus/bukkit/main.java
+++ Bukkit/src/me/redepicness/serverstatus/bukkit/main.java
@@ -1,12 +1,27 @@
 package me.redepicness.serverstatus.bukkit;
 
+import me.redepicness.socketmessenger.api.Data;
+import me.redepicness.socketmessenger.bukkit.SocketAPI;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 public class main extends JavaPlugin{
 
     @Override
     public void onEnable() {
-
+        Bukkit.getScheduler().scheduleSyncRepeatingTask(this, () -> {
+            Data data = new Data();
+            ArrayList<String> players = Bukkit.getOnlinePlayers().stream().map(Player::getName).collect(Collectors.toCollection(ArrayList::new));
+            data.addObject("players", players);
+            data.addInt("playerCount", players.size());
+            data.addString("version", Bukkit.getBukkitVersion());
+            data.addString("ip", Bukkit.getIp());
+            SocketAPI.sendDataToServer("ServerStatus", data);
+        }, 20, 20);
     }
 
 }
--- Bungee/src/me/redepicness/serverstatus/bungee/main.java
+++ Bungee/src/me/redepicness/serverstatus/bungee/main.java
@@ -1,12 +1,39 @@
 package me.redepicness.serverstatus.bungee;
 
+import me.redepicness.socketmessenger.api.Data;
+import me.redepicness.socketmessenger.api.ReceivedDataEvent;
+import net.md_5.bungee.BungeeCord;
+import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.api.plugin.Plugin;
+import net.md_5.bungee.event.EventHandler;
 
-public class main extends Plugin {
+import java.util.ArrayList;
+
+public class main extends Plugin implements Listener {
 
     @Override
     public void onEnable() {
+        BungeeCord.getInstance().getPluginManager().registerListener(this, this);
+    }
 
+    @EventHandler
+    public void onDataReceive(ReceivedDataEvent e){
+        if(e.getChannel().equals("ServerStatus")){
+            Data data = e .getData();
+            ArrayList<String> players = data.getObject("players");
+            int playerCount = data.getInt("playerCount");
+            String bukkitVersion = data.getString("version");
+            String ip = data.getString("ip");
+            BungeeCord.getInstance().broadcast("Server Status update from "+e.getSender()+"!");
+            String pl = "";
+            for(String name : players){
+                pl += ", "+name;
+            }
+            BungeeCord.getInstance().broadcast("Players: "+pl.substring(2));
+            BungeeCord.getInstance().broadcast("Player Count: "+playerCount);
+            BungeeCord.getInstance().broadcast("Bukkit Version: "+bukkitVersion);
+            BungeeCord.getInstance().broadcast("Ip: "+ip);
+        }
     }
 
 }
